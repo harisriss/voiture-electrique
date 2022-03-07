4c8439c6ed2b962e8856739ba7fe057fe0db905f
harisris <haris.coliche@etu.univ-smb.fr>
2022-03-07 15:51:01 +0100

    heroku config

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('build'));
-
-app.listen(process.env.PORT || 3002, () => {
-  console.log(`listening on port ${process.env.PORT || 3002}`);
-});
